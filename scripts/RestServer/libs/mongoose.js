7d6f51cc46170bfc7a9370c17d9e30101e06676d
ostagon <ostagon@mail.ru>
2014-03-06 14:07:25 +0400

    Add configs, try to add some logs, some crud api

--- scripts/RestServer/libs/mongoose.js
+++ scripts/RestServer/libs/mongoose.js
@@ -1,11 +1,18 @@
 /** Mongoose with NodeJS for MongoDB **/
 var mongoose = require('mongoose');
-var options = { user: 'user', pass: 'user'};
-mongoose.connect('mongodb://localhost:27017/greenlist', options);
+
+var config = require("./config");
+var options = config.get('mongoose:options');
+mongoose.connect(config.get('mongoose:uri'), options);
 
 var db = mongoose.connection;
 
+db.on('error', function(err) {
+	//log.error("Connection error: ", err.message);
+});
+
 db.on('open', function() {
+	//log.info("Connected to Mongo");
 	mongoose.connection.db.collectionNames( function(error, names) {
 		if (error) {
 			throw new Error(error);
@@ -15,6 +22,26 @@ db.on('open', function() {
 	});
 });
 
-
-
 //db.close();
+
+/* Schemas */
+var Schema = mongoose.Schema;
+
+var Item = new Schema({
+	name: { type: String, required: true},
+	count: { type: Integer, required: true}
+});
+
+/**/
+Item.path('name').validate( function(value) {
+	return value.length > 0 && value.length < 128;
+});
+
+Item.path('count').validate( function(value) {
+	return value > 0 && value < 1000000;
+});
+/**/
+
+var ItemModel = mongoose.model('Item', Item);
+
+module.exports.ItemModel = ItemModel;
--- scripts/RestServer/server.js
+++ scripts/RestServer/server.js
@@ -1,23 +1,77 @@
 /** Server on NodeJS Express for REST on MongoDB backend **/
-var express = require('express');
-var path  = require('path');
+var express	    = require('express');
+var path		= require('path');
+var config 		= require('./libs/config');
+//var log		 	= require('./libs/log')(module);
+var mongoose 	= require('./libs/mongoose');
 
 var app = express();
 
-//app.use(express.logger('dev'));    // выводим все запросы со статусами в консоль
+app.use(express.logger('dev'));    // выводим все запросы со статусами в консоль
 app.use(express.json());     // стандартный модуль, для парсинга JSON в запросах
 app.use(express.urlencoded());
+app.use(require('formidable')());    // mimeencoded
 //app.use(express.methodOverride()); // поддержка put и delete
 //app.use(app.router);               // модуль для простого задания обработчиков путей
 //app.use(express.static(path.join(__dirname, "public")));
+
+
+// Status 404, 500, error
+/*
+app.use( function(req, res, next) {
+	res.status(404);
+	res.send({ error: "Not found"});
+	return;
+});
+*/
+
+app.use( function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.send({ error: err.message});
+	return;
+});
+
+// error example
+app.get('/error', function(req, res, next) {
+	next(new Error("Random error"));
+});
 
 // rest api
 app.get('/restapi', function (req, res) {
     res.send('API is running');
 });
 
+// *** crud begin ***
+// crud: read: get all items 
+app.get('/restapi/items', function (req, res) {
+    res.send('В процессе разработки');
+});
+
+// crud: create: add new item
+app.post('/restapi/items', function (req, res) {
+    res.send('В процессе разработки');
+});
+
+// crud: read: get item with id
+app.get('/restapi/items/:id', function (req, res) {
+    res.send('В процессе разработки');
+});
+
+// crud: update: update item with id
+app.put('/restapi/items/:id', function (req, res) {
+    res.send('В процессе разработки');
+});
+
+// crud: delete: delete item with id
+app.delete('/restapi/items/:id', function (req, res) {
+    res.send('В процессе разработки');
+});
+// *** crud end ***
+
+
+
 // run server
-var server = app.listen( 1337, function() {
+var server = app.listen( config.get('port'), function() {
 	console.log('Express server on port %d', server.address().port);
 });
 
