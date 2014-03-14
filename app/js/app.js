5358f230fa2d361fc6b3dd021048bf49ecf10a06
ostagon <ostagon@mail.ru>
2014-03-14 13:02:08 +0400

    Free some unnecessary stuff

--- app/js/app.js
+++ app/js/app.js
@@ -9,10 +9,8 @@ angular.module('GreenListApp', [
 	'GreenListApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/view1', 			 {templateUrl: 'views/partial1.html',		  controller: 'MyCtrl1'});
-	$routeProvider.when('/view2',			 {templateUrl: 'views/partial2.html', 		  controller: 'MyCtrl2'});
 	$routeProvider.when('/autocomplete',	 {templateUrl: 'views/autocompleteView.html', controller: 'AutocompleteCtrl'});
 	$routeProvider.when('/simple',			 {templateUrl: 'views/simpleView.html', 	  controller: 'SimpleCtrl'});
 	$routeProvider.when('/greenlist',		 {templateUrl: 'views/greenListView.html', 	  controller: 'GreenListCtrl'});
-	$routeProvider.otherwise({redirectTo: '/view1'});
+	$routeProvider.otherwise({redirectTo: '/greenlist'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,17 +3,9 @@
 /* Controllers */
 
 angular.module('GreenListApp.controllers', []).
-	
-controller('MyCtrl1', [function() {
-
-}]).
-
-controller('MyCtrl2', [function() {
-
-}]).
 
 controller('AutocompleteCtrl', [function() {
-		//$scope.names = ["Аристид", "Фенозон", "Анигар", "Протогорк"];
+		
 }]).
 
 controller('SimpleCtrl', function() {
